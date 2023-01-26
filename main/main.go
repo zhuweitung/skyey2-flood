e665a20bb06739794a3341f0bb09b334b280dff0
zhuweitung <zhuweitung@foxmail.com>
2023-01-26 16:06:29 +0800

    feat:打印错误堆栈

--- main/main.go
+++ main/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/rand"
 	"regexp"
+	"runtime/debug"
 	"strconv"
 	"strings"
 	"time"
@@ -17,7 +18,7 @@ func main() {
 
 	defer func() {
 		if err := recover(); err != nil {
-			log.Fatalln(err)
+			log.Fatalf("err=%v, stack=%s\n", err, string(debug.Stack()))
 		}
 	}()
 
